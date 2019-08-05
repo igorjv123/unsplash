a8190292df1fdf16cda1f6981ef8570ce10e1100
igorjv123 <Ihor_Krutenko@epam.com>
2019-08-05 15:02:24 +0300

    add shadow for cards

---
+++ src/apiKeys.js
@@ -0,0 +1,2 @@
+export const ACCESS_KEY = 'fc6a80d90e77dc9c5176a86a7d2f1b0fca4616fb13c7a316369ca51f6abf9254';
+export const SECRET_KEY = 'a487d6fde28a3ce1b0f83c564273f3b66858d24ba5e38fc2e5436ca260cda67f';
